c98a711f8b09af35aa678dd1bf48948146844006
miguepity <miguel.ardon@unitec.edu>
2018-03-28 03:28:23 -0600

    Agregando enunciado 3 del proyecto de OS redireccionamiento

--- main.cpp
+++ main.cpp
@@ -30,28 +30,105 @@ int main() {
 		cout << ":>>";
 		cin.getline(linea, 20);
 
-		comando = strtok(linea," ");
-		oper = strtok(NULL," ");
-		param = strtok(NULL," ");
-
-		string com1;
-		string com = comando;
-		string delimitator ="-";
+		string com = linea;
+		string delimitator =" -";
+		string delimitator1 = "&";
 		size_t found = com.rfind(delimitator);
-		if (found == 2){ //substring de comando a comando1
-			//comando = strtok(com," ");
-			//comando1 = strtok(NULL," ");
-			cout << com << "  " << com1 << endl;
-			//asigno el string com a comando
-			strcpy(comando, com.c_str());
-		}else	if( strcmp(oper,">") == 0){
-			fd = fopen(param,"w");
-			int fw = open(param, O_APPEND|O_WRONLY);
-			dup2(fw,1);
-			execlp(comando, comando, NULL);
-			close(fw);
-			fclose(fd);
+		size_t found1 = com.rfind(delimitator1);
+
+		if(found == 2 && found1 != 2){
+			comando = strtok(linea," ");
+			subcom = strtok(NULL," ");
+			oper = strtok(NULL," ");
+			param = strtok(NULL," ");
+
+			//si el archivo no existe entonces lo crea y escribe, si ya existe solo sobree escribe en el
+			if( strcmp(oper,">") == 0){
+				fd = fopen(param,"w");
+				int fw = open(param, O_APPEND|O_WRONLY);
+				dup2(fw,1);
+				execlp(comando, comando, subcom, NULL);
+				close(fw);
+				fclose(fd);
+			}else if (strcmp(oper,">>") == 0){ //si el archivo no existe lo crea y escribe, si ya existe le concatena la salida
+				fd = fopen(param,"a");
+				int fw = open(param, O_APPEND|O_WRONLY);
+				dup2(fw,1);
+				execlp(comando, comando, subcom, NULL);
+				close(fw);
+				fclose(fd);
+			}
+
+		}else if(found != 2 && found1 != 2){
+
+			comando = strtok(linea," ");
+			oper = strtok(NULL," ");
+			param = strtok(NULL," ");
+			//si el archivo no existe entonces lo crea y escribe, si ya existe solo sobree escribe en el
+			if( strcmp(oper,">") == 0){
+				fd = fopen(param,"w");
+				int fw = open(param, O_APPEND|O_WRONLY);
+				dup2(fw,1);
+				execlp(comando, comando, NULL);
+				close(fw);
+				fclose(fd);
+			}else if (strcmp(oper,">>") == 0){//si el archivo no existe lo crea y escribe, si ya existe le concatena la salida
+				fd = fopen(param,"a");
+				int fw = open(param, O_APPEND|O_WRONLY);
+				dup2(fw,1);
+				execlp(comando, comando, NULL);
+				close(fw);
+				fclose(fd);
+			}
+
+		}else if(found == 2 && found1 == 2){
+			comando = strtok(linea," ");
+			subcom = strtok(NULL," ");
+			oper = strtok(NULL," ");
+			param = strtok(NULL," ");
+			comando1 = strtok(NULL," ");
+
+			//si el archivo no existe entonces lo crea y escribe, si ya existe solo sobree escribe en el
+			if( strcmp(oper,">") == 0 && strcmp(comando1,"2>&1")){
+				fd = fopen(param,"w");
+				int fw = open(param, O_APPEND|O_WRONLY);
+				dup2(fw,2);
+				execlp(comando, comando, subcom, NULL);
+				close(fw);
+				fclose(fd);
+			}else if (strcmp(oper,">>") == 0 && strcmp(comando1,"2>&1")){ //si el archivo no existe lo crea y escribe, si ya existe le concatena la salida
+				fd = fopen(param,"a");
+				int fw = open(param, O_APPEND|O_WRONLY);
+				dup2(fw,2);
+				execlp(comando, comando, subcom, NULL);
+				close(fw);
+				fclose(fd);
+			}
+
+		}else if(found != 2 && found1 == 2){
+
+			comando = strtok(linea," ");
+			oper = strtok(NULL," ");
+			param = strtok(NULL," ");
+			//si el archivo no existe entonces lo crea y escribe, si ya existe solo sobree escribe en el
+			if( strcmp(oper,">") == 0 && strcmp(comando1,"2>&1")){
+				fd = fopen(param,"w");
+				int fw = open(param, O_APPEND|O_WRONLY);
+				dup2(fw,2);
+				execlp(comando, comando, NULL);
+				close(fw);
+				fclose(fd);
+			}else if (strcmp(oper,">>") == 0 && strcmp(comando1,"2>&1")){//si el archivo no existe lo crea y escribe, si ya existe le concatena la salida
+				fd = fopen(param,"a");
+				int fw = open(param, O_APPEND|O_WRONLY);
+				dup2(fw,2);
+				execlp(comando, comando, NULL);
+				close(fw);
+				fclose(fd);
+			}
+
 		}
+
 		if ((pid = fork()) == 0) {
 			valor = execlp(comando, comando, param, NULL);
 		}else if (valor == -1){
